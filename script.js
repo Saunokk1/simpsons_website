8d899a80ecfde9899113ad4b059a6c37f8e35c70
Saunokk1 <juusohal@hotmail.com>
2025-06-04 14:42:38 +0300

    lets gooo

---
+++ script.js
@@ -0,0 +1,24 @@
+
+function fullPhoto(event) {
+    const content = document.getElementById('content');
+    const photo = event.target;
+
+    let OldContent = content.innerHTML;
+    content.innerHTML = "";
+
+    let a = document.createElement('a')
+    a.innerHTML = "Back"
+    a.id = "jslink"
+    content.appendChild(a)
+
+    let br = document.createElement('br')
+    content.append(br)
+
+    a.addEventListener('click', function () {
+        content.innerHTML = OldContent
+    })
+
+    photo.id = "fullscreen"
+    photo.removeAttribute('onclick')
+    content.appendChild(photo)
+}
